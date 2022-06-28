24d2ce74438b65924fba11aead3002d12c35cead
semetea <55260856+semetea@users.noreply.github.com>
2022-06-28 12:13:20 -0400

    fix

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.User"  # rk
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
