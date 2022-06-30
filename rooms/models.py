ff7e8dd406c999e1336b8d009895e469c2d236ec
semetea <55260856+semetea@users.noreply.github.com>
2022-06-30 15:00:16 -0400

    end admin panel

--- rooms/models.py
+++ rooms/models.py
@@ -91,6 +91,12 @@ class Room(core_models.TimeStampedModel):
     def __str__(self):
         return self.name
 
+    def save(
+        self, *args, **kwargs
+    ):  # Before saving model in database, we can do something
+        self.city = str.capitalize(self.city)
+        super().save(*args, **kwargs)
+
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
