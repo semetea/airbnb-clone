545824bf6700921dbcd5bd64543877fbf8b6b2f7
semetea <55260856+semetea@users.noreply.github.com>
2022-06-30 13:07:37 -0400

    room admin

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,4 @@
+import re
 from xml.sax import default_parser_list
 from django.db import models
 from core import models as core_models
@@ -22,8 +23,12 @@ class Reservation(core_models.TimeStampedModel):
     )
     check_in = models.DateField()
     check_out = models.DateField()
-    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    guest = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
