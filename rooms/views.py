4b1a116a69cfb9f95b4cf9d2e55b2f26524d2890
semetea <55260856+semetea@users.noreply.github.com>
2022-07-04 15:19:50 -0400

    handling exception

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,20 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 # Create your views here.
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(
-        request,
-        "rooms/home.html",
-        context={"rooms": rooms},
-    )
+    paginator = Paginator(
+        room_list, 10, orphans=5
+    )  # orphans -> 다음 페이지에 5개보다 적은 object들이 있다면 전페이지에 넣어라
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            context={"page": rooms},
+        )
+    except EmptyPage:
+        return redirect("/")
