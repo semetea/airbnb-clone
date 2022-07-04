1d5f432ef5f7f1a9194f5e39356197648cad5047
semetea <55260856+semetea@users.noreply.github.com>
2022-07-04 15:41:22 -0400

    Class Based Views

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(
-        room_list, 10, orphans=5
-    )  # orphans -> 다음 페이지에 5개보다 적은 object들이 있다면 전페이지에 넣어라
-    try:
-        rooms = paginator.page(int(page))
-        return render(
-            request,
-            "rooms/home.html",
-            context={"page": rooms},
-        )
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
