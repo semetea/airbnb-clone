dd6d9b01da829e34a18c226f70a63f38baee13f2
semetea <55260856+semetea@users.noreply.github.com>
2022-07-08 15:13:34 -0400

    searchview from

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 # Create your views here.
@@ -9,7 +11,7 @@ class HomeView(ListView):
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
-    ordering = "created"
+    ordering = "name"
     context_object_name = "rooms"
 
     def get_context_data(self, **kwargs):
@@ -31,3 +33,49 @@ class RoomDetail(DetailView):
     """RoomDetail Definition"""
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": str.capitalize(city),
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "room_types": room_types,
+        "countries": countries,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+    return render(
+        request,
+        "rooms/search.html",
+        {**form, **choices},
+    )
