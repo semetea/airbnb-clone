5081f43245b888adeaf1b5942ad34742b6813346
semetea <55260856+semetea@users.noreply.github.com>
2022-07-09 15:28:29 -0400

    login, logout hard way

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 # Create your views here.
@@ -11,14 +12,15 @@ class LoginView(View):
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
 
 
-# same thing as below
-# def login_view(request):
-#     if request.method == "GET":
-#         pass
-
-#     if request.method == "POST":
-#         pass
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
