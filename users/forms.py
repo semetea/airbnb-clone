93f30026e16e2f1b7f6b468c9c32659e117175cf
semetea <55260856+semetea@users.noreply.github.com>
2022-07-09 15:11:54 -0400

    password validation

--- users/forms.py
+++ users/forms.py
@@ -7,13 +7,17 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
     # if you want to valid the field, you should define function name like clean_[field name]
-    def clean_email(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
         try:
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                # should return cleaned_data when you clean function
+                return self.cleaned_data
+            else:
+                # make error belongs to password
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User not found")
-
-    def clean_password(self):
-        return "sdfasf"
+            # make error belongs to email, if you divide clean function, you can just use raise
+            self.add_error("email", forms.ValidationError("User does not exist"))
