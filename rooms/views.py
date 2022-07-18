ed6e96721999f8974712be53fbef423ef885ed37
semetea <55260856+semetea@users.noreply.github.com>
2022-07-18 18:08:30 -0400

    edit photo

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, View, UpdateView
 from django.http import Http404
 from django.shortcuts import redirect, render, reverse
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -180,3 +181,16 @@ def delete_photo(request, room_pk, photo_pk):
         return redirect(reverse("rooms:photos", kwargs={"pk": room_pk}))
     except models.Room.DoesNotExist:
         return redirect(reverse("core:home"))
+
+
+class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
+
+    model = models.Photo
+    template_name = "rooms/photo_edit.html"
+    pk_url_kwarg = "photo_pk"
+    success_message = "Photo Updated"
+    fields = ("caption",)
+
+    def get_success_url(self):
+        room_pk = self.kwargs.get("room_pk")
+        return reverse("rooms:photos", kwargs={"pk": room_pk})
