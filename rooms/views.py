870482c9bfbea99b387e53dab9d60956da91fef1
semetea <55260856+semetea@users.noreply.github.com>
2022-07-19 17:03:00 -0400

    add photo, create room

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, View, UpdateView
+from black import format_cell
+from django.views.generic import ListView, DetailView, View, UpdateView, FormView
 from django.http import Http404
 from django.shortcuts import redirect, render, reverse
 from django.contrib.messages.views import SuccessMessageMixin
@@ -194,3 +195,29 @@ class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateVie
     def get_success_url(self):
         room_pk = self.kwargs.get("room_pk")
         return reverse("rooms:photos", kwargs={"pk": room_pk})
+
+
+class AddPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, FormView):
+    model = models.Photo
+    template_name = "rooms/photo_create.html"
+    fields = ("caption", "file")
+    form_class = forms.CreatePhotoForm
+
+    def form_valid(self, form):
+        pk = self.kwargs.get("pk")
+        form.save(pk)
+        messages.success(self.request, "Photo successfully uploaded")
+        return redirect(reverse("rooms:photos", kwargs={"pk": pk}))
+
+
+class CreateRoomView(user_mixins.LoggedInOnlyView, FormView):
+    form_class = forms.CreateRoomForm
+    template_name = "rooms/room_create.html"
+
+    def form_valid(self, form):
+        room = form.save()
+        room.host = self.request.user
+        room.save()
+        form.save_m2m()
+        messages.success(self.request, "Room successfully created")
+        return redirect(reverse("rooms:detail", kwargs={"pk": room.pk}))
