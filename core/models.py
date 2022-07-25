81488aa813a95de31bfc8666a3d9e63a6790c0e5
semetea <55260856+semetea@users.noreply.github.com>
2022-07-25 15:45:54 -0400

    review

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import managers
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -7,6 +8,7 @@ class TimeStampedModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)  # auto_now_add 새로 만들면 그 시간을 저장
     updated = models.DateTimeField(auto_now=True)  # 모델이 저장될 때마다 시간 기록
+    objects = managers.CustomModelManager()
 
     class Meta:
         abstract = True  # abstract model은 데이터베이스에 등록되지 않는다
